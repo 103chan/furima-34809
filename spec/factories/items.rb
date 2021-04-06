8e5765f7856c9df64eb461ac210884027b939e3a
103chan <koichi.tominaga103@gmail.com>
2021-04-06 12:43:37 +0900

    修正指示２

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     delivery_fee_id  {2}
     prefecture_id    {2}
     days_to_ship_id  {2}
-    price            {'999'}
+    price            {999}
     association :user
 
     after(:build) do |item|
