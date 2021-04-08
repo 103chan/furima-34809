3dd167c27f0cb034147429ab3b65a83b5cd359d4
103chan <koichi.tominaga103@gmail.com>
2021-04-08 18:55:39 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :buildimg, :telephone, :order
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building, :telephone, :order
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
     validates :telephone
