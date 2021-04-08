402ae813d73d5fdc8a853a85795c91be766d8061
103chan <koichi.tominaga103@gmail.com>
2021-04-08 21:38:56 +0900

    バリデーションの編集

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,14 +6,12 @@ class OrderAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :telephone
+    validates :telephone, format: {with: /\A[0-9]+\z/, message: "is invalid."}
   end
-  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-  validates :telephone, format: {with: /\A[\d]\z/, message: "is invalid."}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, telephone: telephone, order_id: order_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, telephone: telephone, order_id: order.id)
   end
 end
