afdcf7366a66fa90d8db5090d92db8cbf00ef160
103chan <koichi.tominaga103@gmail.com>
2021-04-08 21:38:24 +0900

    FactoryBotの設定

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    post_code      { '000-0000' }
+    prefecture_id  { 2 }
+    city           { 'てすと'}
+    address        { 'テスト'}
+    building       { 'テスト'}
+    telephone      { 00000000000 }
   end
 end
