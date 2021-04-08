94c0dcb19aaeb7fa527964dc7349e64a51dab003
103chan <koichi.tominaga103@gmail.com>
2021-04-08 15:02:44 +0900

    データをテーブルに保存する処理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,4 +11,9 @@ class OrderAddress
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   validates :telephone, format: {with: /\A[\d]\z/, message: "is invalid."}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, telephone: telephone, order_id: order_id)
+  end
 end
