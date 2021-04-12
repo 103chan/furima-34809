dcf79cbd8c946587a86dc6784b2283878087309a
103chan <koichi.tominaga103@gmail.com>
2021-04-12 18:01:24 +0900

    Revert "Payjpの機能の実装"
    This reverts commit 266df0d00a19ae620d50d7786b88a59a4b921542.

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  if (!priceInput){ return false;}
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+     const inputValue = priceInput.value;
      const addTaxDom = document.getElementById("add-tax-price");
      addTaxDom.innerHTML = (Math.floor(inputValue / 10));
      const salesProfit = document.getElementById("profit");
